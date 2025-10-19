37d7434b0f5ef4aa5d995a297b1d13699d18f0ce
Rakeshkb6 <kumarrakesh11495@gmail.com>
2025-10-19 13:45:06 +0530

    remove react logo and vite logo

--- rakeshreact/src/App.jsx
+++ rakeshreact/src/App.jsx
@@ -1,22 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Rakesh from './Ab.jsx'
 import Header from './components/header.jsx'
 import Middle from './components/middle.jsx'
 import Contact from './components/contact.jsx'
+import Navbar from './component/navbar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
  
 
   return (
-   <div>
+   
+     <Router>
+    <div>
+
+      <Navbar/>
+      <Routes>
+        <Route path='/' element ={<Header/>}/>
+        <Route path='/header' element ={<Header/>}/>
+        <Route path='/contact' element ={<Contact/>}/>
+        <Route path='/form' element ={<Form/>}/>
+        <Route path='/footer' element ={<Footer/>}/>
+       
+
+        </Routes>
     <Rakesh/>
     <Header/>
     <Middle/>
     <Contact/>
     </div>
+    </Router>
   )
 }
 
